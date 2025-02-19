80e67c6e9e89b21043f4054d03ff9bfb00e6e141
Saad Rasheed <84860156+saadrasheeddev@users.noreply.github.com>
2025-02-19 08:08:25 +0000

    added header to fetch

--- modules/fetch_data_from_url.py
+++ modules/fetch_data_from_url.py
@@ -5,7 +5,12 @@ import pandas as pd
 from .extract_info import extract_info
 
 def fetch_data_from_url(url):
-    response = requests.get(url)
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
         return []
     
